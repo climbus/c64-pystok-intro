eec04adcb622b262f0fe4d355ff98dc1016f03fb
Jarosław Kulesza <jkulesza@grupazpr.pl>
2022-10-01 14:28:53 +0200

    title screen without colors

--- src/screen.asm
+++ src/screen.asm
@@ -1,5 +1,30 @@
 Screen: {
+    .label ScreenPtr = $04
+    .label MapPtr = $06
+    .label ColorPtr = $08
+    .label ColorMapPtr = $0a
+    Row: .byte 25
+
     Draw: {
+        Set16Value(VIC.SCREEN_RAM, ScreenPtr)
+        Set16Value(Map, MapPtr)
+    !Rows:
+        ldy #00
+    !Cols:
+        lda (MapPtr),y
+        sta (ScreenPtr),y
+        
+        iny
+        tya
+        cmp #40
+        bne !Cols-
+
+        Add16Value(ScreenPtr, 40, ScreenPtr)
+        Add16Value(MapPtr, 40, MapPtr)
+
+        dec Row
+        bne !Rows-
+
         rts
     }
 
