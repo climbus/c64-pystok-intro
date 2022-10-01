92939b7090eef1a0cf8915715f0c2feff58fe667
Jarosław Kulesza <jkulesza@grupazpr.pl>
2022-10-01 12:29:24 +0200

    added graphics data

--- src/start.asm
+++ src/start.asm
@@ -1,5 +1,16 @@
 BasicUpstart2(Start)
 
-
 Start:
    rts 
+
+Map:
+	.import binary "./assets/map.bin"
+MapEnd:
+
+Colors:
+	.import binary "./assets/colors.bin"
+
+* = $7000 "Charset"
+Chars:
+	.import binary "./assets/chars.bin"
+CharsEnd:
