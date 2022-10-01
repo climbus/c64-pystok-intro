f8f14948d1d588d92e8884d3e4e18874305becc2
Jarosław Kulesza <jkulesza@grupazpr.pl>
2022-10-01 21:32:59 +0200

    music

---
+++ src/irq.asm
@@ -0,0 +1,41 @@
+IRQ: {
+	.label STABLE_LINE = $10
+
+    Setup: {
+		sei
+		// disable interrupts
+        lda #$7f      // 01111111 
+        sta $dc0d     // turn off CIA timer interrupt
+        sta $dd0d
+        lda $dc0d     // cancel any pending IRQs
+        lda $dd0d    
+
+        lda #$01
+        sta $d01a     // enable VIC-II Raster Beam IRQ
+        
+        lda $d011     // bit 7 of $d011 is the 9th bit of the raster line.
+        and #$7f      // make sure it is set to 0
+        sta $d011
+
+        lda #$35      //turn off the BASIC and KERNAL rom here
+        sta $01     
+		
+        lda #music.startSong-1
+		jsr music.init
+
+        lda #STABLE_LINE
+        sta $d012              // this is the raster line register
+        Set16Value(IRQ.StableIRQ, SYSTEM_IRQ_VECTOR)     // set system IRQ vector to our handler
+        cli           // enable interupts
+        rts
+    }
+
+	StableIRQ: {
+		IRQStart()
+		inc $d020	
+		jsr music.play
+		dec $d020
+        SetRasterInterrupt(STABLE_LINE, StableIRQ)
+		IRQEnd()
+	}
+}
--- src/start.asm
+++ src/start.asm
@@ -1,10 +1,13 @@
+.var music = LoadSid("assets/openyoureyes.sid")	
 BasicUpstart2(Start)
 
 #import "macros.asm"
 #import "vic.asm"
 #import "screen.asm"
+#import "irq.asm"
 
 Start:
+
 
     // Set character memory to $2000
     lda VIC.MEMORY_SETUP_REGISTER
@@ -13,10 +16,15 @@ Start:
     sta VIC.MEMORY_SETUP_REGISTER
     
     jsr Screen.Draw
+    jsr IRQ.Setup
 !:
     jmp !-
+
+*=music.location "Music"
+    .fill music.size, music.getData(i)
 
 * = $2000 "Charset"
 Chars:
 	.import binary "./assets/chars.bin"
 CharsEnd:
+
