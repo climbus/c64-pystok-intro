42b0bc194af632e8cb34700903d8b21ca333fdde
Jarosław Kulesza <jkulesza@grupazpr.pl>
2022-10-01 16:19:01 +0200

    title screen with colors

--- src/screen.asm
+++ src/screen.asm
@@ -6,14 +6,21 @@ Screen: {
     Row: .byte 25
 
     Draw: {
+        lda #00
+        sta VIC.SCREEN_COLOR
+        sta VIC.FRAME_COLOR
+
         Set16Value(VIC.SCREEN_RAM, ScreenPtr)
         Set16Value(Map, MapPtr)
+        Set16Value(VIC.COLOR_RAM, ColorPtr)
     !Rows:
         ldy #00
     !Cols:
         lda (MapPtr),y
         sta (ScreenPtr),y
-        
+        tax
+        lda Colors,x
+        sta (ColorPtr),y
         iny
         tya
         cmp #40
@@ -21,6 +28,7 @@ Screen: {
 
         Add16Value(ScreenPtr, 40, ScreenPtr)
         Add16Value(MapPtr, 40, MapPtr)
+        Add16Value(ColorPtr, 40, ColorPtr)
 
         dec Row
         bne !Rows-
